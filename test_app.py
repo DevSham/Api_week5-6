5aa76714c411954e77d312c94cfa8063d9326331
Nakayiza Shamim <kjingo12@gmail.com>
2019-03-18 18:54:36 +0300

    pick data using json()

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ import json
 def test_all():
     with app.test_client() as T:
         response = T.get('/diary/api/v1/entry')
-        info = json.loads(response.data)
+        info = response.get_json()
         diary = info['diary_entries'][1]['Title']
         assert type(info) == dict
         assert diary == 'Reading'
